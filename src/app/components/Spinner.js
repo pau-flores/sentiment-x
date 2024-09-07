fab2fb24cff8c211529f7f7ad7b5fe978402b572
Paulina Flores <paufr.2822@gmail.com>
2024-09-07 00:36:08 -0700

    Created spinner component to use when loading a page

---
+++ src/app/components/Spinner.js
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-12 h-12 border-4 border-cyan-600 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
